2918d6520a5e8d5dbb6a43598750f1dda5ca5f21
Peeyush0106 <peeyush.agarwal609@gmailcom>
2022-10-03 21:37:40 +0530

    Presentation slides for the project

--- sketch.js
+++ sketch.js
@@ -1,51 +1,77 @@
 var currentQuote = -1;
-var timer = 30;
+var timer = 10;
 
-document.body.style.backgroundColor = "black"
-blink();
-async function blink() {
-    for (var i = 0; i < 10; i++) {
-        await new Promise(r => setTimeout(r, 50));
-        document.body.style.backgroundColor = "white"
-        await new Promise(r => setTimeout(r, 50));
-        document.body.style.backgroundColor = "black"
+var slide = 0;
+
+var bgsDone = ["no", "no", "no", "no", "no"];
+
+updateStartupTimer();
+
+async function updateStartupTimer() {
+    for (var i = 5; i > 0; i--) {
+        document.getElementById("start").innerText = i;
+        await new Promise(r => setTimeout(r, 1000));
     }
+    blackOut();
 }
-document.getElementById("triangle-anim").innerHTML = getTrangleAnimCode();
-document.body.style += getTrangleAnimStyles();
-
-blackOut();
 
 async function blackOut() {
-    await new Promise(r => setTimeout(r, 3000));
     for (var i = 0; i < 1.07; i += 0.07) {
-        await new Promise(r => setTimeout(r, 100));
+        await new Promise(r => setTimeout(r, 30));
+        console.log("iterate")
         document.getElementById("blackout").style.opacity = i;
     }
-    document.getElementById("triangle-anim").innerHTML = ""
     document.getElementById("greet").innerHTML = ""
+    document.getElementById("start").hidden = true;
     document.getElementById("greet-bg").hidden = true;
     document.getElementById("blackout").style.opacity = 0;
-    startConfetti();
-    document.getElementById("quote").hidden = false;
+    // startConfetti();
+    document.getElementById("slides").hidden = false;
     document.getElementById("timer").hidden = false;
+    showSlide();
     updateTimer();
 }
 
 async function updateTimer() {
-    await new Promise(r => setTimeout(r, 999))
+    document.getElementById("timer").innerText = timer;
+    await new Promise(r => setTimeout(r, 799))
     if (timer > 0) {
         timer -= 1;
         document.getElementById("timer").innerText = timer;
         updateTimer();
     }
     else {
-        showThanks();
+        showSlide();
+        timer = 20;
+        updateTimer();
+    }
+}
+
+function showSlide() {
+    if (document.getElementById("slide-" + slide)) document.getElementById("slide-" + slide).hidden = true
+    slide += 1;
+    if (!document.getElementById("slide-" + slide)) showThanks();
+    else {
+        selectImage();
+        document.getElementById("slide-" + slide).hidden = false;
+    }
+}
+
+async function selectImage() {
+    var bgNo = Math.floor(Math.random() * 5) + 1;
+    if (bgsDone[bgNo - 1] === "yes") {
+        selectImage();
+        await new Promise(r => setTimeout(r, 1));
+    }
+    else {
+        bgsDone[bgNo - 1] = "yes";
+        document.getElementById("greet-bg").src = "bg-" + bgNo + ".jpg";
+        document.getElementById("greet-bg").hidden = false;
     }
 }
 
 function showThanks() {
     document.getElementById("timer").hidden = true;
-    document.getElementById("quote").hidden = true;
+    document.getElementById("slides").hidden = true;
     document.getElementById("thanks").hidden = false;
 }
